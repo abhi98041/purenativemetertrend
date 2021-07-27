ebd056ad57aef5e201010d787b46db97e3a79196
abhi98041 <abhi98041@gmail.com>
2021-07-27 08:23:36 +0530

    added query all

--- model/schema.js
+++ model/schema.js
@@ -61,15 +61,52 @@ export const updateMeterName= MeterName => new Promise((resolve, reject) => {
   }).catch((error)=> reject(error));
 });
 
-// export const insertNewMeterReading= newMeterReading => new Promise((resolve, reject) => {
-//   Realm.open(databaseoption).then(realm =>{
-//     realm.write(()=>{
-//       realm.create(METER_READING_SCHEMA, newMeterReading);;
-//       resolve(newMeterReading);
-//     })
-//   }).catch((error)=> reject(error));
-// });
+export const updateMeterReading= MeterReading => new Promise((resolve, reject) => {
+  Realm.open(databaseoption).then(realm =>{
+    realm.write(()=>{
+      let updateMeterReading=realm.objectForPrimaryKey(METER_READING_SCHEMA, MeterReading.id);
+      updateMeterReading.reading=MeterReading.reading;
+      resolve();
+    })
+  }).catch((error)=> reject(error));
+});
 
+
+export const queryAllMeterReading= () => new Promise((resolve, reject) => {
+  Realm.open(databaseoption).then(realm =>{
+      let allMeterReading=realm.objects(METER_READING_SCHEMA);
+      resolve(allMeterReading);
+  }).catch((error)=> reject(error));
+});
+
+
+export const queryAllMeterName= () => new Promise((resolve, reject) => {
+  Realm.open(databaseoption).then(realm =>{
+      let allMeterName=realm.objects(METER_NAME_SCHEMA);
+      resolve(allMeterName);
+  }).catch((error)=> reject(error));
+});
+
+
+export const deleteMeterName= MeterNameID => new Promise((resolve, reject) => {
+  Realm.open(databaseoption).then(realm =>{
+    realm.write(()=>{
+      let deleteMeterName=realm.objectForPrimaryKey(METER_NAME_SCHEMA, MeterNameID);
+      realm.delete(deleteMeterName)
+      resolve();
+    })
+  }).catch((error)=> reject(error));
+});
+
+export const deleteMeterReading= MeterReadingID => new Promise((resolve, reject) => {
+  Realm.open(databaseoption).then(realm =>{
+    realm.write(()=>{
+      let deleteMeterReading=realm.objectForPrimaryKey(METER_READING_SCHEMA, MeterReadingID);
+      realm.delete(deleteMeterReading)
+      resolve();
+    })
+  }).catch((error)=> reject(error));
+});
 
 
 export default new Realm(databaseoption);
--- screens/home.js
+++ screens/home.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { insertNewMeter } from '../model/schema';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, StyleSheet, View, FlatList } from 'react-native';
+import { queryAllMeterName } from '../model/schema';
 import realm from '../model/schema'
+import { FAB, Card, Title, Paragraph,Button, IconButton, Colors, Provider,Portal,Dialog,Modal } from 'react-native-paper';
 
 export default function Home() {
+  const [data, setData] = useState([]);
+reloadData = ()=>{
+  queryAllMeterName().then((MeterName)=>{
+    setData(MeterName);
+  }).catch((error)=>{
+    setData([]);
+    console.error(error);
+  })
+}
+
     return (
       <Text>Home</Text>
     );
